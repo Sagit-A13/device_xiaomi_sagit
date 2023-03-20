672196903ad7250e47d1cbb54be104c5caba9695
deny2311 <deny2311@gmail.com>
2023-03-20 03:55:14 +0000

    sagit: Use libhidlbase from vndk v32 for libvendor.goodix.hardware.interfaces.biometrics.fingerprint@2.1.so

--- extract-files.sh
+++ extract-files.sh
@@ -20,6 +20,11 @@ function blob_fixup() {
         vendor/lib64/vendor.goodix.hardware.fingerprintextension@1.0.so)
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
+    	# Use VNDK 32 libhidlbase
+    	vendor/lib64/libvendor.goodix.hardware.interfaces.biometrics.fingerprint@2.1.so)
+            "${PATCHELF_0_8}" --remove-needed "libhidlbase.so" "${2}"
+            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+    	    ;;
     esac
 }
 

3ab9e2362e7c88e944234a732db45ee23931e2f5
TRISF <trisfaizal@gmail.com>
2023-03-27 22:22:30 +0000

    cherishify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_sagit.mk
+    $(LOCAL_DIR)/cherish_sagit.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_sagit-user \
-    evolution_sagit-userdebug \
-    evolution_sagit-eng
+    cherish_sagit-user \
+    cherish_sagit-userdebug \
+    cherish_sagit-eng

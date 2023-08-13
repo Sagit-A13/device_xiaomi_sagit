bbb2783f0149098351c746762527bc5e5cb93ddc
deny2311 <deny2311@gmail.com>
2023-08-13 04:11:10 +0000

    sagit: Derp ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_sagit.mk
+    $(LOCAL_DIR)/derp_sagit.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_sagit-user \
-    cherish_sagit-userdebug \
-    cherish_sagit-eng
+    derp_sagit-user \
+    derp_sagit-userdebug \
+    derp_sagit-eng
